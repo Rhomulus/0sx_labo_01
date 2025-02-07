129af28558fb1a30f162f6332adbcbe57ac908ce
Rhomulus <delomesounou@gmail.com>
2025-02-07 15:01:23 -0500

    arduinoTp01

--- 0sx_labo_01/0sx_labo_01.ino
+++ 0sx_labo_01/0sx_labo_01.ino
@@ -1,9 +1,46 @@
+const int LED_PIN = 13;
+int clignonte = 5;  
+const int DUREE_VARIATION = 2048;  // a diviser par 256 pour delaie
+const int ATTENTE_ALLUMAGE = 2000;
+const int ATTENTE_CLIGNONTEMENT= 250;
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(9600);  // initialise les series de communication
 }
+void allumer() {
+  Serial.println("Allume – 6225272");
+  digitalWrite(LED_PIN, HIGH);
+  delay(ATTENTE_ALLUMAGE);
+};
+
+void clignonter() {
+  Serial.println("Clignontement–6225272");
+  for (int i = 0; i < clignonte; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(ATTENTE_CLIGNONTEMENT);
+    digitalWrite(LED_PIN, LOW);
+    delay(ATTENTE_CLIGNONTEMENT);
+  }
+};
+
+void variation() {
+  Serial.println("Variation - 6225272");
+  int delaiVariation = (DUREE_VARIATION/256);
+  for (int i = 0; i <= 255; i++) {
+    analogWrite(LED_PIN, i);
+    delay(delaiVariation);
+  }
+};
+
+void eteint() {
+  Serial.println("Éteint - 6225272");
+  digitalWrite(LED_PIN, LOW);
+  delay(ATTENTE_ALLUMAGE);
+};
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-}
+  allumer();
+  clignonter();
+  variation();
+  eteint();
+};
